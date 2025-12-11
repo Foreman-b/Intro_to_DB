8850f20b632561b9f57a875538f85f79fb6c98a0
Foreman-b <hamzat4life@gmail.com>
2025-12-11 15:11:59 +0100

    Add MySQLServer file

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+from mysql.connector import connect, Error
+from getpass import getpass
+
+
+try:
+    with connect(
+        host = "localhost",
+        user = input("Enter username: "),
+        password = getpass("Enter password: "),
+        ) as connection:
+        create_database = "CREATE DATABASE IF NOT EXISTS alx_book_store;"
+
+        with connection.cursor() as cursor:
+            cursor.execute(create_database)
+            connection.commit()
+            print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(e)
+    
+connection.close()
+
+
